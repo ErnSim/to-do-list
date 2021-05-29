e02daf620092bc1ae9e16aa98ea28028704cf33c
Ernest Simlat <74451245+ErnSim@users.noreply.github.com>
2021-05-30 00:20:50 +0200

    load task function + some bug with last index of task

--- to_do_list.js
+++ to_do_list.js
@@ -1,24 +1,66 @@
+// Inital tasks (localStorage in the future)
+let task = {		// może spróbować to jako tablicę, by móc odczytać ostatni index
+	0: {
+		"isCheck": true,
+		"content": "Coś tam bredzę sobie"
+	},
+	1: {
+		"isCheck": false,
+		"content": "Abba ojcze"
+	},
+	5: {
+		"isCheck": true,
+		"content": "papiesz wapiesz"
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Declare variables
 let textInput = document.querySelector('#text-input');
 let submitButton = document.querySelector('#submit-button');
 let clearListButton = document.querySelector('#clear-list-button')
 let listOfTodos = document.querySelector('#list-of-todos')
-let todoIndex = 0;
+let taskIndex = Object.keys(task).length;					// znaleść inny sposób na znalezienie ostatniego indexu
 
+// Execute functions
+loadTheTasks();
 submitButton.addEventListener('click', addTodo);
 clearListButton.addEventListener('click', clearList);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
+function loadTheTasks(){
+	for (let i = 0; i < Object.keys(task).length; i++) {	// inny sposób na ostatni index
+		// typeof myVar !== 'undefined'
+
+		if (task[i]["isCheck"]) {
+			listOfTodos.innerHTML += `	<div id="index-${ i }" class="task-box">
+										<input type="checkbox" id="checkbox-${ i }" checked>
+										<p> ${ task[i]["content"] } </p>
+										<div class="remove-task-button no-select" onclick="removeTask(${ i })">remove task</div>
+									</div>`;
+		}
+		else {
+			listOfTodos.innerHTML += `	<div id="index-${ i }" class="task-box">
+										<input type="checkbox" id="checkbox-${ i }">
+										<p> ${ task[i]["content"] } </p>
+										<div class="remove-task-button no-select" onclick="removeTask(${ i })">remove task</div>
+									</div>`;
+		}
+
+	}
+}
+
 function addTodo() {
 	let content = textInput.value;
 
 	if (content != "") {
-		listOfTodos.innerHTML += `	<div id="index-${ todoIndex }" class="task-box">
-										<input type="checkbox" id="">
+		listOfTodos.innerHTML += `	<div id="index-${ taskIndex }" class="task-box">
+										<input type="checkbox" id="checkbox-${ taskIndex }">
 										<p> ${ content } </p>
-										<div class="remove-task-button no-select" onclick="removeTask(${ todoIndex })">remove task</div>
+										<div class="remove-task-button no-select" onclick="removeTask(${ taskIndex })">remove task</div>
 									</div>`;
-		todoIndex++;
+		taskIndex++;
 		console.log(content);
 	}
 }
